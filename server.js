6aba35ea6c64d50fafb0218608de56880daf8a47
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-10-28 09:57:00 -0700

    server.js added express node module added

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 4000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+  mongoose
+    .connect("mongodb://localhost/mujiba", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => {
+      console.log("DB Connected: " + "mongodb://localhost/mujiba");
+    })
+    .catch((err) => {
+      console.log("Unable to Connect to DB" + err);
+    });
+});
