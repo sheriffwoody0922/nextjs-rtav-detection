33adea08fa411272a9d55afa50bf98e1e23fb361
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-11-27 23:14:15 -0800

    index.tsx performance improve

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import { useRouter } from "next/router";
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import InputIcon from '@mui/icons-material/Input';
-import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
-
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -15,21 +10,14 @@ export default function Home() {
     <div className={styles.container}>
 
       <main className={styles.main}>
-        <Box
-          component="form"
-          sx={{
-           m: 4, width: '300px'
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <Button className={styles.logobutton} onClick={()=>router.push('/auth/login')} variant="contained" endIcon={<InputIcon />}>
-            Login 
-          </Button>
-          <Button className={styles.logobutton} onClick={()=>router.push('/auth/register')} variant="contained" endIcon={<AppRegistrationIcon />}>
-            Register 
-          </Button>
-        </Box>
+        <div className="w-64">
+          <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 py-2 px-4 rounded" onClick={()=>router.push('/auth/login')}>
+            Login
+          </button>
+          <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 py-2 px-4 rounded" onClick={()=>router.push('/auth/register')}>
+            Register
+          </button>
+        </div>
       </main>
     </div>
   )
