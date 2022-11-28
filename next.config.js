3a4f5d9860de566129d32a85c75bf3214bfabf7b
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-11-28 08:06:32 -0800

    next config revert

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+
+
+/*
 module.exports = {
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
@@ -10,3 +20,5 @@ module.exports = {
     return config
   },
 }
+
+*/
