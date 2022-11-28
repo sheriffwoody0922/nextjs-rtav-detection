05395a20c93da50c837ab4e6ab17e48a7768a9fa
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-11-28 06:23:53 -0800

    mainboard.tsx changed

--- pages/common/mainboard.tsx
+++ pages/common/mainboard.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import sessionProps from "../../next-middlewares/sessionProps";
 import Common from "../../layouts/Common";
 
-function Mainboard (props:any) {
+export default function Mainboard (props:any) {
 
     return (
-        <>
-            <Common layoutData={props.layoutData}></Common>
-        </>
+        <Common layoutData={props.layoutData}>
+            <div></div>
+        </Common>
     )
 }
-
-export default Mainboard;
 
 
 export const getServerSideProps = async function (context:any) {
