9a9e15b8900b3c84a11f299688abee3b3595cf3e
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-11-28 10:55:19 -0800

    sameSite remove

--- next-middlewares/sessionOptions.js
+++ next-middlewares/sessionOptions.js
@@ -1,7 +1,6 @@
 const sessionOptions = {
   cookieName: process.env.SESSION_COOKIE_NAME,
   cookieOptions: {
-    sameSite: true,
     maxAge: 3600,
   },
   password: process.env.APPLICATION_SECRET,
