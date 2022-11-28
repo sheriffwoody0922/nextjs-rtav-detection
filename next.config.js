8c99d8bffd4f00b1f258d3d5cbc477e5b3943767
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-11-28 00:09:44 -0800

    preact node module added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+    return config
+  },
 }
-
-module.exports = nextConfig
