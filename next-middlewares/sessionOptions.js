0be39a8e6680e28158a5766efeccac20fb233975
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-11-28 10:43:41 -0800

    secure -> remove

--- next-middlewares/sessionOptions.js
+++ next-middlewares/sessionOptions.js
@@ -1,7 +1,6 @@
 const sessionOptions = {
   cookieName: process.env.SESSION_COOKIE_NAME,
   cookieOptions: {
-    // secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: true,
     maxAge: 3600,
   },
