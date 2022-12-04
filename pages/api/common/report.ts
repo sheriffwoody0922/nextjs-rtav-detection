6741039b928dad0f4a56f9f2a80264c0459f8151
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-12-04 11:56:54 -0800

    some reporttype get error changed!

--- pages/api/common/report.ts
+++ pages/api/common/report.ts
@@ -1,5 +1,6 @@
 import User from "../../../models/user";
 import Car from "../../../models/car";
+import ReportType from "../../../models/reporttype";
 import Report from "../../../models/report";
 import formidable from "formidable";
 import fs from "fs";
@@ -20,17 +21,18 @@ const handler = async function handler(req:any, res:any) {
       console.log(fields);
       const path = await saveFile(files.file)
 
+      let typeinfo = await ReportType.findOne({_id: fields.reporttype});
       User.findOne({email: fields.useremail,}).exec(
         function (err, result) {
             // Tada! random user
             let report = new Report({
                 reportowner:result._id,
-                reporttype:fields.reporttype,
+                reporttype:typeinfo._id,
                 reportvideo:path, 
                 reportgps:`${faker.address.latitude()} ${faker.address.longitude()}`,
                 reportdate:new Date(),
                 reportedcar:`${faker.vehicle.manufacturer()} ${faker.vehicle.model()}`,
-                reportfine:10,
+                reportfine:typeinfo.typeprice,
                 sendedwhatsapp:"",
                 reportflag:"new"
             });
