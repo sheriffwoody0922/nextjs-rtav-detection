9bfece2c35262d06cf09a92bcf23a0132f16f57b
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-12-04 08:33:33 -0800

    image and video upload function add!

--- components/Common/ReportAdd.tsx
+++ components/Common/ReportAdd.tsx
@@ -2,12 +2,13 @@ import React, {useState, ChangeEvent} from "react"
 import {toast } from "react-toastify";
 
 interface FileInfoType{
-    video:string,
+    media:string,
     currentfile:object,
+    filetype:string
 }
 
 export default function ReportAdd(props:any){
-    const [data, setdata] = useState<FileInfoType>({video:"", currentfile:{}})
+    const [data, setdata] = useState<FileInfoType>({media:"", currentfile:{}, filetype:""})
 
     const selectvideo = (event:ChangeEvent<HTMLInputElement>) => {
 
@@ -20,14 +21,17 @@ export default function ReportAdd(props:any){
     
         if (!fileInput.files || fileInput.files.length === 0) {
           toast.error("Files list is empty");
-          setdata({video:"", currentfile:{}})
+          setdata({media:"", currentfile:{}, filetype:""})
           return;
         }
     
         const file = fileInput.files[0];
 
         const path = URL.createObjectURL(file);
-        setdata({video:path, currentfile:file});
+
+        const filetype = file.type
+
+        setdata({media:path, currentfile:file, filetype:filetype});
     }
 
     const uploadvideo = () =>{
@@ -36,15 +40,17 @@ export default function ReportAdd(props:any){
     return(
         <div className="w-100">
             <div className="w-100 flex justify-between">
-                    <input className="font-bold my-4" accept="video/*" type="file" onChange={(e) => selectvideo(e)}/>
-                {data.video&&
+                    <input className="font-bold my-4" accept="video/*, image/*" type="file" onChange={(e) => selectvideo(e)}/>
+                {data.media&&
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-36 my-auto" onClick={()=>uploadvideo()}>
                         Upload
                     </button>
                 }
             </div>
             <div className="w-100 mt-2">
-                <video className="max-w-3xl w-5/6 mx-auto" src={data.video} controls></video>
+                {
+                    data.filetype.includes("video")?<video className="max-w-3xl w-5/6 mx-auto" src={data.media} controls/>:<img className="max-w-3xl w-5/6 mx-auto" src={data.media}/>
+                }
             </div>
         </div>
     )
