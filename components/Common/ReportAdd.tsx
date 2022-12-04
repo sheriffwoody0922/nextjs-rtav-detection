a6fc57d39c6ee89f5bb54fa4f5867431193d7a8c
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-12-04 09:38:14 -0800

    DropDown UI Add!

--- components/Common/ReportAdd.tsx
+++ components/Common/ReportAdd.tsx
@@ -37,21 +37,28 @@ export default function ReportAdd(props:any){
     const uploadvideo = () =>{
         props.uploadfile(data.currentfile)
     }
-    return(
-        <div className="w-100">
+    return( <div className="w-100">
             <div className="w-100 flex justify-between">
-                    <input className="font-bold my-4" accept="video/*, image/*" type="file" onChange={(e) => selectvideo(e)}/>
-                {data.media&&
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-36 my-auto" onClick={()=>uploadvideo()}>
+                <input className="font-bold my-auto py-2 pl-1" accept="video/*, image/*" type="file" onChange={(e) => selectvideo(e)}></input>
+                {data.media&&<div className="flex">
+                    <select id="reporttype" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-auto">
+                        <option>Dangerous driving</option>
+                        <option>Traffic light not obeyed</option>
+                        <option>Illegal Overtake</option>
+                        <option>Illegal stopping</option>
+                        <option>Failure to stop after accident</option>
+                    </select>
+                    <button className="ml-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-36 my-auto" onClick={()=>uploadvideo()}>
                         Upload
                     </button>
+                </div>
                 }
+
             </div>
             <div className="w-100 mt-2">
                 {
                     data.filetype.includes("video")?<video className="max-w-3xl w-5/6 mx-auto" src={data.media} controls/>:<img className="max-w-3xl w-5/6 mx-auto" src={data.media}/>
                 }
             </div>
-        </div>
-    )
+    </div>)
 }
