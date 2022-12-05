f0a1572e39e4934883e2d768a8bdc19c6beb8e55
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-12-04 23:50:30 -0800

    reportype open

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,5 +1,5 @@
 import User from "../../../models/user";
-// import ReportType from "../../../models/reporttype";
+import ReportType from "../../../models/reporttype";
 import _ from "lodash";
 import bcrypt from "bcryptjs";
 import loginValidator from "../../../validators/auth/loginValidator";
@@ -20,12 +20,12 @@ const handler = async function handler(req:any, res:any) {
       user.password
     );
 
-    // let reporttype = new ReportType({
-    //   typename:"Dangerous driving",
-    //   typeprice:20
-    // })
+    let reporttype = new ReportType({
+      typename:"Dangerous driving",
+      typeprice:20
+    })
 
-    // await reporttype.save();
+    await reporttype.save();
 
     if (!validPassword)
       return res.status(400).send({ password: "Invalid password" });
