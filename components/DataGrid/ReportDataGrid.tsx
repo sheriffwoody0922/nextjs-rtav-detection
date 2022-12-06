27f75121665eaaf25b221ac81f649418accfe2b7
sheriffwoody0922 <codewarrior0922@gmail.com>
2022-12-05 23:02:51 -0800

    search bar changed!

--- components/DataGrid/ReportDataGrid.tsx
+++ components/DataGrid/ReportDataGrid.tsx
@@ -50,10 +50,6 @@ function EditToolbar(props: EditToolbarProps) {
           <Chip className={`super-app-theme--${item.current} m-1`} label={item.current} key={item.current} />)
         }
       </div>
-      <div className='w-full flex'>
-        <input type="text" id="first_name" className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Cars" />
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-      </div>
     </GridToolbarContainer>
   );
 }
